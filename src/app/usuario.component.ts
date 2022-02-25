50a51f3a836712bbd89018f29840872787ba6155
Cristian López Ramirez <cl827209@gmail.com>
2022-02-25 16:56:01 -0600

    13. ¿Cómo trabajan los IF y FOR en Angular?

--- src/app/usuario.component.ts
+++ src/app/usuario.component.ts
@@ -7,5 +7,17 @@ import { Component } from "@angular/core";
 
 export class UsuarioComponent {
     
+    usuarios:string[] = ['Luis', 'Fernando', 'María', 'Jorge'];
     usuarioNombre:string = "Cristian López";
+    visible:boolean = false;
+
+    constructor() {
+        setTimeout(() => {
+            this.visible = true;
+        }, 3000);
+    }
+
+    onAgregarUsuario() {
+        this.usuarios.push(this.usuarioNombre);
+    }
 }
