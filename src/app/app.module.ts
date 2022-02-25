abc433581de3c14f00c432eb93de61098992faa9
Cristian López Ramirez <cl827209@gmail.com>
2022-02-25 16:37:25 -0600

    11. Ejecutar proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// My components
+import { UsuarioComponent } from "./usuario.component";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
